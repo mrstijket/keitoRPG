4f297311e6beac9ee38cab72e6c7df2a4cea70a8
mrstijket <mr.stijket@gmail.com>
2022-12-16 13:30:56 +0300

    Health Bar

---
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace RPG.Attributes
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] Health healthComponent = null;
+        [SerializeField] RectTransform foregroundBar = null;
+        [SerializeField] Canvas rootCanvas = null;
+        void Update()
+        {
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0) ||
+                Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
+            foregroundBar.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
+        }
+    }
+}
