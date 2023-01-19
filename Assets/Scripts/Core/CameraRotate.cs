554a60dbded17efd4b3ee6d51524d4da8a7003b5
mrstijket <mr.stijket@gmail.com>
2023-01-20 01:52:12 +0300

    Rotation to camera

---
+++ Assets/Scripts/Core/CameraRotate.cs
@@ -0,0 +1,27 @@
+using Cinemachine;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraRotate : MonoBehaviour
+    {
+        [SerializeField] CinemachineVirtualCamera targetCam;
+        [SerializeField] Transform camTarget;
+
+        [SerializeField] float rotationSpeed = 20f;
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                targetCam.transform.RotateAround(camTarget.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+                Camera.main.transform.LookAt(camTarget);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                targetCam.transform.RotateAround(camTarget.transform.position, Vector3.down, rotationSpeed * Time.deltaTime);
+                Camera.main.transform.LookAt(camTarget);
+            }
+        }
+    }
+}
